e69405a677f301e561ddde60695a9b63ab9dd186
piotr-napadlek <fisheye@gazeta.pl>
2016-01-24 21:03:33 +0100

    prepared a test for a shuffler

--- src/main/java/pl/cardgames/spit/shuffler/Shuffler.java
+++ src/main/java/pl/cardgames/spit/shuffler/Shuffler.java
@@ -3,5 +3,5 @@ package pl.cardgames.spit.shuffler;
 import java.util.Collection;
 
 public interface Shuffler<T> {
-	public Collection<T> shuffle(Collection<T> items);
+	public boolean shuffle(Collection<T> items);
 }
---
+++ src/test/java/pl/cardgames/spit/shuffler/ShufflerTest.java
@@ -0,0 +1,50 @@
+package pl.cardgames.spit.shuffler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ShufflerTest {
+
+	private Shuffler<Integer> shuffler;
+	
+	@Before
+	public void setUp() {
+		// assign shuffler here
+		// ie. shuffler = new ShufflerImpl();
+	}
+	
+	@Test
+	@Ignore // delete when implemented
+	public void shouldShufflePortionOfInts() {
+		// given
+		Collection<Integer> ints = IntStream.range(3, 1105).boxed().collect(Collectors.toList());
+		Collection<Integer> intsSorted = new ArrayList<>(ints);
+		// when
+		boolean changed = shuffler.shuffle(intsSorted);
+		// then
+		Assert.assertTrue(changed);
+		Assert.assertNotNull(intsSorted);
+		Assert.assertFalse(ints.equals(intsSorted));
+	}
+	
+	@Test
+	@Ignore
+	public void shouldHandleShufflingSingleInt() {
+		// given
+		Collection<Integer> ints = IntStream.range(1, 2).boxed().collect(Collectors.toList());
+		Collection<Integer> intsSorted = new ArrayList<>(ints);
+		// when
+		boolean changed = shuffler.shuffle(intsSorted);
+		// then
+		Assert.assertTrue(changed);
+		Assert.assertNotNull(intsSorted);
+		Assert.assertTrue(ints.equals(intsSorted));
+	}
+}
