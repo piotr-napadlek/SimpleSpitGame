329cfa5b9424efb6ef2c45f585cc0f635a0ced55
piotr-napadlek <fisheye@gazeta.pl>
2016-03-02 00:08:19 +0100

    introduced first svg converted to fxml and prototype of card dragging

--- spit-gui/src/main/java/pl/cardgames/gui/controller/MainViewController.java
+++ spit-gui/src/main/java/pl/cardgames/gui/controller/MainViewController.java
@@ -1,7 +1,30 @@
 package pl.cardgames.gui.controller;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.layout.AnchorPane;
 import pl.cardgames.gui.controller.util.ControllerBase;
 
 public class MainViewController extends ControllerBase {
 
+	@FXML
+	private AnchorPane cardContainer;
+
+	@FXML
+	private void initialize() throws IOException {
+		cardContainer.getChildren().add(
+				FXMLLoader.load(this.getClass().getResource("/pl/cardgames/gui/img/ad.fxml")));
+		cardContainer.setMaxHeight(88);
+		cardContainer.setPrefHeight(88);
+		cardContainer.autosize();
+		
+		cardContainer.setOnMouseDragged(mouseEvent -> {
+			cardContainer.setCursor(Cursor.MOVE);
+			cardContainer.setLayoutX(mouseEvent.getSceneX());
+			cardContainer.setLayoutY(mouseEvent.getSceneY());
+		});
+	}
 }
